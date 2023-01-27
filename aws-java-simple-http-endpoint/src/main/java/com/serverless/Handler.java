033d45750214c602c021226d19f7a18f8934b25d
Martin Schaef <schaef@amazon.com>
2023-01-27 13:31:34 -0500

    getting dg gradle example ready

--- aws-java-simple-http-endpoint/src/main/java/com/serverless/Handler.java
+++ aws-java-simple-http-endpoint/src/main/java/com/serverless/Handler.java
@@ -1,29 +1,33 @@
 package com.serverless;
 
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.apache.log4j.Logger;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import org.apache.log4j.Logger;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+import java.io.IOException;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
 	private static final Logger LOG = Logger.getLogger(Handler.class);
 
 	@Override
-	public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+	public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
 		LOG.info("received: " + input);
-		Response responseBody = new Response("Hello, the current time is " + new Date());
-		Map<String, String> headers = new HashMap<>();
-		headers.put("X-Powered-By", "AWS Lambda & Serverless");
-		headers.put("Content-Type", "application/json");
-		return ApiGatewayResponse.builder()
-				.setStatusCode(200)
-				.setObjectBody(responseBody)
-				.setHeaders(headers)
-				.build();
+
+    createProcessNoncompliant(input.getBody());
+
+		return new APIGatewayProxyResponseEvent().withStatusCode(200).withBody("Hi");
 	}
+
+  public void createProcessNoncompliant(String taintedInput) {
+    // Noncompliant: user-supplied parameter is passed to an OS command and could be malicious.
+    ProcessBuilder pb = new ProcessBuilder("/usr/local/bin/program", "--color", taintedInput);
+    try {
+      pb.start();
+    } catch (IOException e) {
+      System.out.println(e);
+    }
+  }
 }
